e1e7cb1c11479e5dc4835980d9e52ff38ebd5ec0
basyobasyo <om1997518@gmail.com>
2022-01-12 11:06:11 +0900

    Userモデルの設定、内部結合のため

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :another_user, class_name: "User",
+  foreign_key: "pair_id"
+
+  belongs_to :pair, class_name: "User", optional: true
 end
