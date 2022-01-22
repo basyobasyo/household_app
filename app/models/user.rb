95e96a3a00dbd6aa047d7c5efa30b29ec97e0059
basyobasyo <om1997518@gmail.com>
2022-01-22 11:17:24 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # アソシエーションの記述
   has_one :another_user, class_name: 'User',
                          foreign_key: 'pair_id'
 
   belongs_to :pair, class_name: 'User', optional: true
+  # // アソシエーションの記述
+
+  # バリデーションの記述
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }, on: :save  #saveメソッドのみ適応。followメソッドの際にこのバリデーションがかかってしまうため、updateメソッドを使うことができない
+  validates :nickname, presence: true
+  validates :email   , uniqueness: true  
+  validates :pair_id , uniqueness: true
+  # // バリデーションの記述
+  
+
 
   def self.follow(follow_id, another_id)
     user = User.find(follow_id)
-    user.pair_id = another_id
+    # user.pair_id = another_id
     another_user = User.find(another_id)
-    another_user.pair_id = follow_id
-
-    user.save
-    another_user.save
+    # another_user.pair_id = follow_id
+    user.update(pair_id: another_id)
+    another_user.update(pair_id: follow_id)
   end
 end
