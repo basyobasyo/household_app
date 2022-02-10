aa840728a60b8e68a357bcf637cb0190e4946161
basyobasyo <om1997518@gmail.com>
2022-02-10 14:21:32 +0900

    カテゴリーの追加、コードの修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,14 @@ class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '-' },
     { id: 2, name: '食費' },
-    { id: 3, name: '交通費' }
+    { id: 3, name: '交通費' },
+    { id: 4, name: '日用品' },
+    { id: 5, name: '衣服' },
+    { id: 6, name: '医療費' },
+    { id: 7, name: '光熱費' },
+    { id: 8, name: '居住費' },
+    { id: 9, name: '趣味・遊び' },
+    { id: 10, name: 'その他' }
   ]
 
   include ActiveHash::Associations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  # セルフバリデーションによる記述
   require 'validator/pair_id_check_validator'
+  # // セルフバリデーションによる記述
 
+  # deviseによる記述
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # // deviseによる記述
 
   # アソシエーションの記述
   has_many :payments
@@ -21,10 +25,12 @@ class User < ApplicationRecord
   validates :pair_id, uniqueness: true, pair_id_check: true, on: :update # 新規登録の際にバリデーションが動作しないようにupdateの際にのみ設定
   # // バリデーションの記述
 
+  # フォロー機能のメソッド
   def self.follow(follow_id, another_id)
     user = User.find(follow_id)
     another_user = User.find(another_id)
     user.update(pair_id: another_id)
     another_user.update(pair_id: follow_id)
   end
+    # // フォロー機能のメソッド
 end
