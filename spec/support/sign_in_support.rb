1a14c9a0b45396b63e91eed76c52a960e9020229
basyobasyo <om1997518@gmail.com>
2022-02-23 16:15:14 +0900

    rubocopの実行

--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -2,12 +2,12 @@ module SignInSupport
   def sign_in(user)
     # ログイン画面へ移動し、必要情報の入力、投稿ボタンの押下
     visit new_user_session_path
-    fill_in "Email", with: user.email
-    fill_in "パスワード", with: user.password
+    fill_in 'Email', with: user.email
+    fill_in 'パスワード', with: user.password
     find('input[name="commit"]').click
     # 遷移後のページがトップページになっているか
     expect(current_path).to eq(root_path)
     # トップページにログインユーザーの名前があるか
-    expect(page).to have_content("#{user.nickname}")
+    expect(page).to have_content(user.nickname.to_s)
   end
 end
